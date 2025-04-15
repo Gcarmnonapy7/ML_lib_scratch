d0e28d46f34540ca7cb61797fe58a8fcc7281cbd
Gabriel Carmona <gbcarmona05@gmail.com>
2025-04-15 15:50:37 -0300

    Implementation of NaiveBayes and refactory of the RegressionModel adding BaseRegression

--- src/classification/logistic_regression.py
+++ src/classification/logistic_regression.py
@@ -1,31 +1,8 @@
 import numpy as np
+from src.base_regression.base_regression import BaseRegression
 #The logistic regression is very similar to the linear regression(implementation // not the function)
-class LogisticRegression:
-    def __init__(self,lr=0.001,epochs=1000):
-        self.lr = lr
-        self.epochs = epochs
-        self.weights = None
-        self.bias = None
-    def fit (self,x,y):
-        '''
-        :return: "stored" the data
-        '''
-        n_samples,n_features = x.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-
-        for i in range(self.epochs):
-            linear_model = np.dot(x,self.weights) + self.bias
-            y_hat = self.sigmoid(linear_model)
-
-            #derivatives of weights and bias
-            dw = (1/n_samples) * np.dot(x.T,(y_hat- y))
-            db = (1/n_samples) * np.sum(y_hat- y)
-
-            #uptade the weights and bias
-            self.weights -= self.lr * dw
-            self.bias -= self.lr * db
-
+class LogisticRegression(BaseRegression):
+    
     def predict(self,X):
         """
         return the predict of the x_test in 0 or 1
--- src/score/accuracy.py
+++ src/score/accuracy.py
@@ -1,4 +1,3 @@
 import numpy as np
 def accuracy (y_true,y_hat):
-    accuracy = np.sum(y_true == y_hat) / len(y_true)
-    return accuracy
+    return np.sum(y_hat == y_true) / len(y_true)
